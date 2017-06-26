47278aed970f2ff930da1127fb012e5be1f5c242
Ally Mahmoud <ally.h.mahmoud.19@dartmouth.edu>
2017-06-26 16:30:55 +0200

    my first button

---
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Ally Mahmoud on 6/26/17.
+//  Copyright © 2017 Ally Mahmoud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
